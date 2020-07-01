c61dc95962bea4e42e88a318805ad7922b1bbe3c
sergioss1205 <66535410+sergioss1205@users.noreply.github.com>
2020-07-01 12:27:53 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,32 @@
+$(document).ready(function(){
+    $('[data-modal=consultation]').on('click', function() {
+        $('.overlay, #consultation').fadeIn('slow');
+    });
+    $('.modal__close').on('click', function() {
+        $('.overlay, #consultation').fadeOut('slow');
+    });
+
+    $('input[name=phone]').mask("999 999-99-99");
+
+    $('form').submit(function(e) {
+        e.preventDefault();
+        $.ajax({
+            type: "POST",
+            url: "mailer/smart.php",
+            data: $(this).serialize()
+        }).done(function() {
+            $(this).find("input").val("");
+            $('#consultation').fadeOut();
+            $('.overlay, #thanks').fadeIn('slow');
+            
+            $('form').trigger('reset');
+        });
+        return false;
+    });
+
+
+    
+   
+
+
+});
